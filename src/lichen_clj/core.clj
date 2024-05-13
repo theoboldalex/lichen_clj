d572a45c9c612a0a3f43ee4b7ea67aad914c63f1
theoboldalex <theoboldalex@gmail.com>
2024-05-13 20:34:33 +0100

    Prints options to stdout

--- src/lichen_clj/core.clj
+++ src/lichen_clj/core.clj
@@ -1,27 +1,33 @@
 (ns lichen-clj.core
   (:require [clojure.java.io :as io]
-            [clojure.data.json :as json])
+            [clojure.data.json :as json]
+ [clojure.string :as string])
   (:gen-class))
 
 (defn make-http-req [url]
   (with-open [reader (io/reader (io/input-stream (java.net.URL. url)))]
     (apply str (line-seq reader))))
 
+(def prompt (slurp "welcome_prompt.txt"))
+
 (def ^:private licenses
   "https://api.github.com/licenses")
 
 (defn get-available-licenses []
-  (make-http-req licenses))
+  (json/read-str (make-http-req licenses)))
 
 (defn get-license [l]
   (make-http-req (str licenses "/" (get l "key"))))
 
-(defn -main
-  "A simple OSS License Generator"
-  [& args]
-  (println
+(defn list-options []
   (->>
    (get-available-licenses)
-   (json/read-str)
-   (first)
-   (get-license))))
+   (map #(get % "name"))
+   (map-indexed #(str (str (inc %1) ". ") %2))
+   (string/join "\n")))
+
+(defn -main
+  "A simple OSS License Generator"
+  [& args] 
+  (println prompt)
+ (println (list-options)))
