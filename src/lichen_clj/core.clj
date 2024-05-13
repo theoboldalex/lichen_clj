8069bbca9ae7fd0041852cee88c556b1630469f1
theoboldalex <theoboldalex@gmail.com>
2024-05-13 21:32:41 +0100

    It writes the license

--- src/lichen_clj/core.clj
+++ src/lichen_clj/core.clj
@@ -10,24 +10,39 @@
 
 (def prompt (slurp "welcome_prompt.txt"))
 
-(def ^:private licenses
+(def ^:private licenses_url
   "https://api.github.com/licenses")
 
 (defn get-available-licenses []
-  (json/read-str (make-http-req licenses)))
+  (json/read-str (make-http-req licenses_url)))
+
+(def licenses (get-available-licenses))
 
 (defn get-license [l]
-  (make-http-req (str licenses "/" (get l "key"))))
+  (json/read-str (make-http-req (str licenses_url "/" (get l "key")))))
 
 (defn list-options []
   (->>
-    (get-available-licenses)
+   licenses
   (map #(get % "name"))
   (map-indexed #(str (str (inc %1) ". ") %2))
   (string/join "\n")))
+
+(defn get-choice []
+  (print "\nSelect a license number: ")
+  (flush)
+  (let [choice (read-line)
+        l (licenses (dec (Integer/parseInt choice)))]
+    (println (str (get l "name") "! Nice!"))
+    ((get-license l) "body")))
+
+(defn write-license [body]
+  (spit "LICENSE" body))
 
 (defn -main
   "A simple OSS License Generator"
   [& args]
   (println prompt)
-  (println (list-options)))
+  (println (list-options))
+ (let [choice (get-choice)]
+   (write-license choice)))
